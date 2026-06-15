3864e7ae82aa484cca654be99599a89da4e715a3
Evando Pereira De Oliveira <seu_email@exemplo.com>
2026-06-14 22:45:51 -0300

    feat: drag and drop between kanban columns

--- src/app/pages/ticket-list/ticket-list.ts
+++ src/app/pages/ticket-list/ticket-list.ts
@@ -1,17 +1,18 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Ticket } from '../../models/ticket.model';
 import { TicketService } from '../../services/ticket';
 import { RouterModule } from '@angular/router';
+import { DragDropModule, CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-ticket-list',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, DragDropModule],
   templateUrl: './ticket-list.html',
   styleUrl: './ticket-list.css'
 })
-export class TicketList {
+export class TicketList implements OnInit {
   tickets: Ticket[] = [];
 
   constructor(private ticketService: TicketService) {
@@ -19,7 +20,27 @@ export class TicketList {
     
   }
 
-  getByStatus(status:string): Ticket[] {
-    return this.tickets.filter(ticket => ticket.status === status);
+  abertos: Ticket[] = [];
+  emAndamento: Ticket[] = [];
+  fechados: Ticket[] = [];
+
+ngOnInit() {
+  const todos = this.ticketService.getTickets();
+  this.abertos = todos.filter(t => t.status === 'aberto');
+  this.emAndamento = todos.filter(t => t.status === 'em_andamento');
+  this.fechados = todos.filter(t => t.status === 'fechado');
+}
+
+drop(event: CdkDragDrop<Ticket[]>) {
+  if (event.previousContainer === event.container) {
+    moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+  } else {
+    transferArrayItem(
+      event.previousContainer.data,
+      event.container.data,
+      event.previousIndex,
+      event.currentIndex
+    );
   }
+}
 }
